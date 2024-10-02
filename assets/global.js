459f23c13a4fd200c7984584acb818deffa97a57
Lea Verou <lea@verou.me>
2024-10-02 14:13:36 -0400

    Move scripts to global.js

---
+++ assets/global.js
@@ -0,0 +1,8 @@
+import Color from "https://colorjs.io/color.js";
+
+import "https://elements.colorjs.io/src/color-scale/color-scale.js";
+import "https://elements.colorjs.io/src/color-chart/color-chart.js";
+// import "../node_modules/color-elements/src/color-scale/color-scale.js";
+// import "../node_modules/color-elements/src/color-chart/color-chart.js";
+
+let charts_h2 = document.querySelector("h2#charts");
