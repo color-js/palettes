62d2360d468ded57b245bb158d2bbda57f9de1fa
Dmitry Sharabin <dmitrysharabin@gmail.com>
2026-03-24 12:08:42 +0100

    Make it work after Color Elements switched to Nudeps

--- assets/global.js
+++ assets/global.js
@@ -1,7 +1,7 @@
 import Color from "https://colorjs.io/color.js";
 
-import "https://elements.colorjs.io/src/color-scale/color-scale.js";
-import "https://elements.colorjs.io/src/color-chart/color-chart.js";
+// import "https://elements.colorjs.io/src/color-scale/color-scale.js";
+// import "https://elements.colorjs.io/src/color-chart/color-chart.js";
 // import "../node_modules/color-elements/src/color-scale/color-scale.js";
 // import "../node_modules/color-elements/src/color-chart/color-chart.js";
 
