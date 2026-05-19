dc17ee0347e5153ea0716fbbb707eaa6d3a12dd7
Dmitry Sharabin <dmitrysharabin@gmail.com>
2026-05-19 18:23:06 +0200

    Charts with lightness X-axis (#13)
    * Use `Lightness` channel on X-axis
    
    * Switch to `<space-picker>` instead of hand-writtern one
    
    * Address Lea's feedback
    
    - Import the needed components only
    - Set properties before the elements are defined
    - Use TLA

--- assets/global.js
+++ assets/global.js
@@ -1,89 +1,67 @@
-import Color from "https://colorjs.io/color.js";
-
-// import "https://elements.colorjs.io/src/color-scale/color-scale.js";
-// import "https://elements.colorjs.io/src/color-chart/color-chart.js";
-// import "../node_modules/color-elements/src/color-scale/color-scale.js";
-// import "../node_modules/color-elements/src/color-chart/color-chart.js";
-
 let params = new URL(location.href).searchParams;
 
-let charts_h2 = document.querySelector("h2#charts");
-let scales_h2 = document.querySelector("h2:is(#tints, #palettes):has(+ table color-scale)");
+let chartsPicker = document.querySelector("h2#charts space-picker");
+let scalesPicker = document.querySelector(
+	"h2:is(#tints, #palettes) space-picker",
+);
+let pickers = [chartsPicker, scalesPicker].filter(Boolean);
 
-if (charts_h2 || scales_h2) {
-	const defaultSpace = "oklch";
-	let spaceId = params.get("space") ?? defaultSpace;
-	let spaces = {polar: {}, rectangular: {}};
-	for (let id in Color.spaces) {
-		let space = Color.spaces[id];
+let charts =
+	chartsPicker?.parentElement.parentElement.querySelectorAll("color-chart");
+let scales =
+	scalesPicker?.parentElement.nextElementSibling.querySelectorAll(
+		"color-scale",
+	);
 
-		if (space.id !== id) {
-			continue;
-		}
-		let isPolar = space.coords.h?.type === "angle";
-		spaces[isPolar ? "polar" : "rectangular"][id] = space;
-	}
-
-	let select = document.createElement("select");
-	select.innerHTML = Object.entries(spaces).map(([type, spaces]) => `
-		<optgroup label="${ type }">
-			${ Object.entries(spaces).map(([id, space]) => `<option value="${ id }">${ space.name }</option>`).join("\n") }
-		</optgroup>
-	`).join("\n");
-	select.value = spaceId;
-
-	let select2;
-
-	if (charts_h2) {
-		// Filter color spaces by polar
-		charts_h2.prepend(select, " ");
-		let charts = charts_h2.parentElement.querySelectorAll("color-chart");
-
-		select.onchange = evt => {
-			spaceId = select.value;
-			params.set("space", spaceId);
-			console.log(params.toString());
-			history.pushState(null, "", `?${ params }${ location.hash }`);
-			let selectedSpace = Color.Space.all.find((space) => space.id === spaceId);
-			let coords = Object.entries(selectedSpace.coords);
-			let info = coords.map(([id, meta]) => `${ (meta.name ?? id)[0] }: ${ spaceId }.${ id }`).join(", ");
-
-			for (let i = 0; i < 3; i++) {
-				charts[i].y = spaceId + "." + coords[i][0];
-				charts[i].info = info;
-			}
-
-			if (select2 && select2.value !== spaceId) {
-				select2.value = spaceId;
-				select2.dispatchEvent(new Event("change"));
-			}
-		}
-	}
-
-	if (scales_h2) {
-		select2 = charts_h2 ? select.cloneNode(true) : select;
-		select2.value = spaceId;
-		scales_h2.prepend(select2, " ");
-		let scales = scales_h2.nextElementSibling.querySelectorAll("color-scale");
-
-		select2.onchange = evt => {
-			spaceId = select2.value;
-			let selectedSpace = Color.Space.all.find((space) => space.id === spaceId);
-			let coords = Object.entries(selectedSpace.coords);
-			let info = coords.map(([id, meta]) => `${ (meta.name ?? id)[0] }: ${ spaceId }.${ id }`).join(", ");
-
-			for (let scale of scales) {
-				scale.info = info;
-			}
-
-			if (select && select.value !== spaceId) {
-				select.value = spaceId;
-				select.dispatchEvent(new Event("change"));
-			}
-		}
-	}
-
-	// Trigger change events to update the charts and scales so they reflect the specified color space
-	select?.dispatchEvent(new Event("change"));
-	select2?.dispatchEvent(new Event("change"));
+function groupBy (space) {
+	let isPolar = space.coords.h?.type === "angle";
+	return isPolar ? "Polar" : "Rectangular";
 }
+
+function update (source) {
+	let spaceId = source.value;
+	let coords = Object.entries(source.selectedSpace.coords);
+	let hasL = coords.some(([id]) => id === "l");
+	let info = coords
+		.map(([id, meta]) => `${(meta.name ?? id)[0]}: ${spaceId}.${id}`)
+		.join(", ");
+
+	params.set("space", spaceId);
+	history.pushState(null, "", `?${params}${location.hash}`);
+
+	if (charts) {
+		for (let i = 0; i < 3; i++) {
+			charts[i].y = `${spaceId}.${coords[i][0]}`;
+			charts[i].x = hasL && coords[i][0] !== "l" ? `${spaceId}.l` : null;
+			charts[i].info = info;
+		}
+	}
+
+	for (let scale of scales ?? []) {
+		scale.info = info;
+	}
+
+	for (let picker of pickers) {
+		if (picker !== source && picker.value !== spaceId) {
+			picker.value = spaceId;
+		}
+	}
+}
+
+let spaceId = params.get("space");
+
+for (let picker of pickers) {
+	picker.groupBy = groupBy;
+	if (spaceId) {
+		picker.value = spaceId;
+	}
+	picker.addEventListener("spacechange", () => update(picker));
+}
+
+await Promise.all(
+	["space-picker", "color-chart", "color-scale"].map((tag) =>
+		customElements.whenDefined(tag),
+	),
+);
+
+pickers[0] && update(pickers[0]);
